c28f8c44667c8f79ca9055cfb870075b86b38ca3
sob-alex <a.sobolev@azoft.com>
2021-09-27 15:49:29 +0700

    deploying settings

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,15 +3,7 @@ import { Paper, Box } from '@material-ui/core';
 const Dashboard = () => {
   return (
     <Box mt={4}>
-      <Paper
-        style={{
-          height: '400px',
-          padding: '10px 20px'
-        }}
-        elevation={1}
-      >
-        <h1>Dashboard</h1>
-      </Paper>
+      asd
     </Box>
   );
 };
